1d6d250fb4a04ab42d5d6db574a9597c30a377ac
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-07-15 17:34:14 -0700

    Few changes on portfolio, moving

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,26 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields: [ 
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+                {
+            name: 'imageurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hotspot: true,
+            }
+        },
+                {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        }
+
+    ]
+}
---
+++ frontend_react/src/index.js
@@ -0,0 +1,7 @@
+import react from 'react';
+import reactDom from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
