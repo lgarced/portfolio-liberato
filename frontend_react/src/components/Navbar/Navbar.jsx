15dd3770228a6106c0337324e0db4dde48506d30
Liberato Garced <93692943+lgarced@users.noreply.github.com>
2022-07-17 10:34:56 -0700

    Navbardone

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -25,21 +25,24 @@ import { images } from '../../constants'
            <HiMenuAlt4 onClick ={ () => setToggle(true) }/>
           {
             toogle &&  (
-              <motion.div>
+              <motion.div
                 whileInView={ { x: [300, 0] } }
                 transition={{ duration: 0.85, ease: "easeInOut" }}
+                >
                 <HiX onClick ={ () => setToggle(false) }/>
-                    {['Home','About', 'Projets', 'Skills', 'Contact'].map( (item) => (
-                   <li key={`link-${item}`}> 
-                      <a href={`#${item}`} onClick ={ () => setToggle(false) }> {item} </a>
+                <ul>    
+                    {['HOME','ABOUT', 'PROJECTS', 'SKILS', 'CONTACT'].map( (item) => (
+                   <li key={item}> 
+                      <a href={`#${item}`} onClick ={ () => setToggle(false) }> 
+                      {item}
+                       </a>
                     </li>
                     ))}
+                   </ul> 
               </motion.div>
-            )
-          }
+            )}
       </div>
     </nav>
-  )
-}
+  )}
 
 export default Navbar
--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -1,9 +1,11 @@
 import mechina from '../assets/mechina.jpeg';
 import Liberato_Garced from '../assets/Liberato_Garced.png';
+import bgWhite from '../assets/bgWhite.png';
 
 
 export default {
     mechina,
     Liberato_Garced,
+    bgWhite
 
 }
